80566e8ad67206495ba49b4179cbb2987a0bc725
yangtc <494150123@qq.com>
2019-07-19 15:08:02 +0800

    自动生成代码

---
+++ back/src/main/java/com/heeexy/example/controller/WechatUserController.java
@@ -0,0 +1,21 @@
+package com.heeexy.example.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author yangtengcan
+ * @since 2019-07-19
+ */
+@RestController
+@RequestMapping("/wechatUser")
+public class WechatUserController {
+
+}
+
